6593969905a04eb2e6737434a243aeeede0854a7
Paul Lathrop <paul@simplegeo.com>
2011-02-23 12:37:50 -0800

    Remove cruft.

--- clive/config.py
+++ clive/config.py
@@ -2,8 +2,6 @@ from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 LOG_FILE = '/dev/null'
 LOG_LEVEL = WARNING
-LOG_MAX_BYTES = 100000000
-LOG_COUNT = 5
 
 POD_DIR = '/Users/plathrop/tmp/pod'
 POD_TIMEOUT = 15

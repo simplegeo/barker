5a9b8226778b312f061a064b02c3f625f517e80f
Paul Lathrop <paul@simplegeo.com>
2011-03-22 18:50:49 +0000

    Install os pod when installing as debian package.

--- clive/config.py
+++ clive/config.py
@@ -3,7 +3,7 @@ from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 LOG_FILE = '/dev/null'
 LOG_LEVEL = WARNING
 
-POD_DIR = '/Users/plathrop/tmp/pod'
+POD_DIR = '/usr/share/clive/pods'
 POD_TIMEOUT = 15
 
 CONSOLE_LOG = True

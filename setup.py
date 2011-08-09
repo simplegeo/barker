398330964fd6578e80108df187eb27f48ca28d16
Simple Geebus <nerds+geebus@simplegeo.com>
2011-08-09 18:44:46 +0000

    Prepared changelog for build 68

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='barker',
-      version="0.1.67",
+      version="0.1.68",
       description='Barker is a hive mind for your clusters.',
       author='Paul Lathrop',
       author_email='paul@simplegeo.com',
